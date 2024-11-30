5c7e6f14720a8b979afefa68c46da9a011035989
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-30 12:27:58 +0600

    fix some error

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-   <div className="center">
-    <h1 className="text-3xl font-bold text-brand">OwnDrive - Its your own google drive</h1>
+   <div className="flex-center h-screen">
+    <h1 className="h1">OwnDrive - Its your own google drive</h1>
    </div>
   );
 }
