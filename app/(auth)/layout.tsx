cd71b503f636d309edbc40b8122d37ca645660f5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-11-30 21:15:54 +0600

    add some text with some design on layout page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image'
 import React from 'react'
 
-const Layout = () => {
+const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <div>Layout</div>
+    <section className='flex min-h-screen'>
+   <div className='bg-brand'>
+    <div>
+        <Image
+        src='/favicon.ico'
+        alt='logo'
+        width={16}
+        height={16}
+        className='h-auto'
+        />
+        <div className='space-y-5 text-white'>
+
+            <h1 className='h1'>Manage your files the best way</h1>
+            <p className='body-1'>This is a place where you can manage all your documents </p>
+
+        </div>
+    </div>
+   </div>
+    </section>
   )
 }
 
