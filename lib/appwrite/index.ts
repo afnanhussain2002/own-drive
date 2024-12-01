e823ad21d98dd3c6957e86eec28bfff1c90b191f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-01 08:41:12 +0600

    get the session on a session variable

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,12 +1,16 @@
 import { Client } from "node-appwrite";
 import { appwriteConfig } from "./config";
+import { cookies } from "next/headers";
 
 export const createSessionClient = async () => {
   const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
     .setProject(appwriteConfig.projectId);
+    const session = (await cookies()).get('appwrite-session');
+
+    
 };
 
 export const createAdminClient = async() =>{
-    
+
 }
