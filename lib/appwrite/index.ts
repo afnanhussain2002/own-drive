2ac9b0ea5e2b8c978946de8d925796955c32ab10
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-01 08:45:09 +0600

    return two func

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -1,6 +1,8 @@
-import { Client } from "node-appwrite";
+import { Account, Client, Databases } from "node-appwrite";
 import { appwriteConfig } from "./config";
 import { cookies } from "next/headers";
+import { get } from "http";
+import { Database } from "lucide-react";
 
 export const createSessionClient = async () => {
   const client = new Client()
@@ -10,6 +12,15 @@ export const createSessionClient = async () => {
 
     if (!session || !session.value) throw new Error('No session found');
     client.setSession(session.value);
+
+    return{
+        get account(){
+            return new Account(client);
+        },
+        get database(){
+            return new Databases(client);
+        }
+    }
 
 export const createAdminClient = async() =>{
 
