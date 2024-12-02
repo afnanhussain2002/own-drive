76f5353ebd685e830c49df46255d32d26bf66877
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-02 20:27:26 +0600

    add set key on create admin

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -8,36 +8,38 @@ export const createSessionClient = async () => {
   const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
     .setProject(appwriteConfig.projectId);
-    const session = (await cookies()).get('appwrite-session');
+  const session = (await cookies()).get("appwrite-session");
 
-    if (!session || !session.value) throw new Error('No session found');
-    client.setSession(session.value);
+  if (!session || !session.value) throw new Error("No session found");
+  client.setSession(session.value);
 
-    return{
-        get account(){
-            return new Account(client);
-        },
-        get database(){
-            return new Databases(client);
-        }
-    }
-}
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+  };
+};
 
-export const createAdminClient = async() =>{
-    const client = new Client()
+export const createAdminClient = async () => {
+  const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
-    .setProject(appwriteConfig.projectId);
-    const session = (await cookies()).get('appwrite-session');
+    .setProject(appwriteConfig.projectId)
+    .setKey(appwriteConfig.secretKey);
 
-    if (!session || !session.value) throw new Error('No session found');
-    client.setSession(session.value);
+  const session = (await cookies()).get("appwrite-session");
 
-    return{
-        get account(){
-            return new Account(client);
-        },
-        get database(){
-            return new Databases(client);
-        }
-    }
-}
+  if (!session || !session.value) throw new Error("No session found");
+  client.setSession(session.value);
+
+  return {
+    get account() {
+      return new Account(client);
+    },
+    get database() {
+      return new Databases(client);
+    },
+  };
+};
