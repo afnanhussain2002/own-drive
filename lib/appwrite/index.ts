eab536f305fb74bce9694b1f991deb834cbcad15
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-02 20:26:38 +0600

    write code for create admin client

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -21,7 +21,23 @@ export const createSessionClient = async () => {
             return new Databases(client);
         }
     }
+}
 
 export const createAdminClient = async() =>{
+    const client = new Client()
+    .setEndpoint(appwriteConfig.endpointUrl)
+    .setProject(appwriteConfig.projectId);
+    const session = (await cookies()).get('appwrite-session');
 
+    if (!session || !session.value) throw new Error('No session found');
+    client.setSession(session.value);
+
+    return{
+        get account(){
+            return new Account(client);
+        },
+        get database(){
+            return new Databases(client);
+        }
+    }
 }
