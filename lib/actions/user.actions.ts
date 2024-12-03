9c3cb6af0383038db7ea4a8d62d3654f92e8ba38
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-03 23:21:00 +0600

    destructure the database from admin client

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,7 +1,10 @@
 "use server";
 
+import { createAdminClient } from "../appwrite";
+
 const getUserByEmail = async (email: string) => {
-    
+   const {database} = await createAdminClient();
+   
 }
 
 const createAccount = async ({fullName,email}: {fullName: string, email: string}) => {
