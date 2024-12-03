c62703f9bf40a6177bf943fdd4272d24e17f0cad
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-03 23:23:53 +0600

    complete the get user email func

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,10 +1,19 @@
 "use server";
 
+import { Query } from "node-appwrite";
 import { createAdminClient } from "../appwrite";
+import { appwriteConfig } from "../appwrite/config";
 
 const getUserByEmail = async (email: string) => {
    const {database} = await createAdminClient();
-   
+
+   const result = await database.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("email", [email])]
+
+   )
+   return result.total > 0 ? result.documents[0] : null;
 }
 
 const createAccount = async ({fullName,email}: {fullName: string, email: string}) => {
