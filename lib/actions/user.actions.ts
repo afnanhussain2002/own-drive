9c85964e5bbfe96f5a645fb5d8b2f6c334149e5f
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-12-03 23:45:12 +0600

    save the user data for auth

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -40,5 +40,19 @@ const createAccount = async ({fullName,email}: {fullName: string, email: string}
     
  if(!accountId) throw new Error("Failed to send OTP");
 
+ if (!existingUser) {
+    const {database} = await createAdminClient();
+    await database.createDocument(
+        appwriteConfig.databaseId,
+        appwriteConfig.usersCollectionId,
+        ID.unique(),
+        {
+            fullName,
+            email,
+            avatar:'https://media.istockphoto.com/vectors/default-profile-picture-avatar-photo-placeholder-vector-illustration-vector-id1223671392?k=20&m=1223671392&s=170667a&w=0&h=kEAA35Eaz8k8A3qAGkuY8OZxpfvn9653gDjQwDHZGPE=',
+            accountId
+        }
+    )
+ }
  
 };
